3f5e566a3194666a2b05b4b092ef7a482ee82b88
EmmanuelBlas-eng <emmanuelblasroberto251@gmail.com>
2026-07-29 13:06:32 +0800

    fix: update temple data with new entries and adjust filtering criteria

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -20,6 +20,63 @@ menuButton.addEventListener('click', () => {
 
 const temples = [
 
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    area: 11500,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+  },
+  {
+    templeName: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    area: 74792,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+  },
+  {
+    templeName: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    area: 96630,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+  },
+  {
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    area: 6861,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+  },
+  {
+    templeName: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    area: 156558,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+  },
+  {
+    templeName: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    area: 9600,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+  },
+  {
+    templeName: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1983, December, 2",
+    area: 116642,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+  },
+
     {
     templeName: "Manila Philippines",
     location: "Manila, Philippines",
@@ -45,81 +102,7 @@ const temples = [
     imageUrl:
     "https://churchofjesuschristtemples.org/assets/img/temples/urdaneta-philippines-temple/urdaneta-philippines-temple-45874-main.jpg"
   },
-  {
-    templeName: "Alabang Philippines",
-    location: "Alabang, Philippines",
-    dedicated: "2026, January, 18",
-    area: 35998,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/alabang-philippines-temple/alabang-philippines-temple-65306-main.jpg"
-  },
-  {
-    templeName: "Davao Philippines",
-    location: "Davao, Philippines",
-    dedicated: "2026, May, 3",
-    area: 18450,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/davao-philippines-temple/davao-philippines-temple-69513-main.jpg"
-  },
-  {
-    templeName: "Bacolod Philippines",
-    location: "Bacolod, Philippines",
-    dedicated: "2026, May, 31",
-    area: 27895,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/bacolod-philippines-temple/bacolod-philippines-temple-70237-main.jpg"
-  },
-  {
-    templeName: "Lima Perú",
-    location: "Lima, Perú",
-    dedicated: "1986, January, 10",
-    area: 9600,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/lima-peru-temple/lima-peru-temple-12721-main.jpg"
-  },
-  {
-    templeName: "Deseret Peak Utah",
-    location: "Deseret Peak, Utah",
-    dedicated: "2024, November, 10",
-    area: 71998,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/deseret-peak-utah-temple/deseret-peak-utah-temple-54400-main.jpg"
-  },
-
-  {
-    templeName: "Mexico City Mexico",
-    location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
-    area: 116642,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/mexico-city-mexico-temple/mexico-city-mexico-temple-4060-main.jpg"
-  },
-  {
-    templeName: "Orlando Florida",
-    location: "Orlando, Florida",
-    dedicated: "1994, October, 9-11",
-    area: 70000,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/orlando-florida-temple/orlando-florida-temple-51938-main.jpg"
-  },
-
-    {
-    templeName: "Paris France Temple",
-    location: "Paris, France",
-    dedicated: "2017, May 21",
-    area: 69600,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2056-main.jpg"
-  },
-
-    {
-    templeName: "Sydney Australia Temple",
-    location: "Sydney, Australia",
-    dedicated: "1984, September, 20-23",
-    area: 30067,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/sydney-australia-temple/sydney-australia-temple-43342-main.jpg"
-  },
+ 
 
 
 ]
@@ -168,13 +151,13 @@ newTempleLink.addEventListener("click", (e) => {
 largeTempleLink.addEventListener("click", (e) => {
     e.preventDefault();
     setActiveNavLink(largeTempleLink);
-    createTempleCard(temples.filter(temple => temple.area >= 30000));
+    createTempleCard(temples.filter(temple => temple.area >=90000));
 });
 
 smallTempleLink.addEventListener("click", (e) => {
     e.preventDefault();
     setActiveNavLink(smallTempleLink);
-    createTempleCard(temples.filter(temple => temple.area <= 30000));
+    createTempleCard(temples.filter(temple => temple.area <=10000));
 });
 
 
@@ -195,6 +178,7 @@ function createTempleCard(filteredTemples) {
         img.setAttribute("src", temple.imageUrl);
         img.setAttribute("alt", `${temple.templeName} Temple`);
         img.setAttribute("loading", "lazy");
+        img.className = "temple-preview";
 
         card.appendChild(name);
         card.appendChild(location);
